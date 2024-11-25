1c3af675ba033765d1449a91fe409c550bf03dfc
Konstantin Göksu <konstantin.goksu@elev.ga.ntig.se>
2024-11-25 09:53:30 +0100

    nästan klar

---
+++ Näsan steg 3 övning 7/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+class Program
+{
+static void Main(string[] args)
+    {
+        string word;
+        bool choice = false;
+        int counter = 3;
+        List<string> excel = new List<string>();
+
+for(int e=0; e<3; ++e)
+        {
+            Console.Clear();
+            Console.WriteLine("Skriv in ett ord.");
+            word = Console.ReadLine();
+                excel.Add(word);  
+
+        }
+        Console.Clear();
+        
+        for(int i=0; i<excel.Count; ++i)
+        {
+            Console.WriteLine($"Word {i + 1}: {excel[i]}");
+            Console.ReadLine();
+        }
+
+    }
+
+
+
+
+static int GetChoice(int userchoice)
+{
+    return userchoice;
+}
+
+
+
+    
+}
