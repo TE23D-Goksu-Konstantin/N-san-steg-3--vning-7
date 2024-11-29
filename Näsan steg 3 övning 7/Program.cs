aaf18e9ed9f4fc052782cde6dabd29cd75c5e8ce
Konstantin Göksu <konstantin.goksu@elev.ga.ntig.se>
2024-11-29 10:46:29 +0100

    klar, tror jag

--- Näsan steg 3 övning 7/Program.cs
+++ Näsan steg 3 övning 7/Program.cs
@@ -6,32 +6,44 @@ static void Main(string[] args)
 {
 
 int userPick = GetChoice("Björk", "Ek", "Tall");
-Console.WriteLine($"Du valde {userPick}");
+if(userPick == 1)
+{
+    Console.WriteLine($"Du valde nummer {userPick}, Björk");
+}else if(userPick == 2)
+{
+    Console.WriteLine($"Du valde nummer {userPick}, Ek");
+}else
+{
+    Console.WriteLine($"Du valde nummer {userPick}, Tall");
+}
+Console.ReadLine();
 
 }
 
 static int GetChoice(string pick1, string pick2, string pick3)
-{
-    int pick;
+    {
+    int pick = 0;
     bool Clearing = false;
 
     while(!Clearing)
-    {
+        {
         Console.WriteLine("Välj en av följande alternativ genom att skriva dess siffra");
         Console.WriteLine("1:" + pick1);
         Console.WriteLine("2:" + pick2);
         Console.WriteLine("3:" + pick3);
-    }
+    
 
     string Input = Console.ReadLine();
 
-    if(int.TryParse(Input, out pick) && pick <= 3 && pick &&)
+    if(int.TryParse(Input, out pick) && pick <= 3 && pick >=1)
     {
         Clearing = true;
     }else{
-        Console.WriteLine("Fel, skriv in en siffra");
-    }
+        Console.Clear();
+        Thread.Sleep(100);
+        Console.WriteLine("Fel, försök igen");
+            }
+        }
     return pick;
-}
-
+    }
 }
